e64bfc03ac289e7e41652784b642a1d4de7d6957
Abel Gómez <abgolla@gmail.com>
2025-09-02 22:20:56 +0200

    Add UTF-8 as option when running from cmd.exe

--- es.sistedes.library.manager/execute.bat
+++ es.sistedes.library.manager/execute.bat
@@ -27,6 +27,8 @@ REM
 
 setlocal
 
+set JAVA_OPTS=-Dfile.encoding=UTF-8
+
 CALL execute.env.bat
 
 CALL mvn package
@@ -47,21 +49,21 @@ GOTO END
 :INIT
 SET COMMON_OPTS=init -y %YEAR% -P %HANDLE_PREFIX%
 ECHO Initializing JISBD
-java -jar target/%JAR% %COMMON_OPTS% -a %JISBD% -i "%INPUT_DIR%/%JISBD%" -o "%OUTPUT_DIR%/%JISBD%" -p %JISBD_PDF_PATTERN% -x %JISBD_XLSX% %JISBD_INIT_ARGS% || GOTO FAIL
+java %JAVA_OPTS% -jar target/%JAR% %COMMON_OPTS% -a %JISBD% -i "%INPUT_DIR%/%JISBD%" -o "%OUTPUT_DIR%/%JISBD%" -p %JISBD_PDF_PATTERN% -x %JISBD_XLSX% %JISBD_INIT_ARGS% || GOTO FAIL
 ECHO Initializing JCIS
-java -jar target/%JAR% %COMMON_OPTS% -a %JCIS%  -i "%INPUT_DIR%/%JCIS%"  -o "%OUTPUT_DIR%/%JCIS%"  -p  %JCIS_PDF_PATTERN% -x %JCIS_XLSX%  %JCIS_INIT_ARGS%  || GOTO FAIL
+java %JAVA_OPTS% -jar target/%JAR% %COMMON_OPTS% -a %JCIS%  -i "%INPUT_DIR%/%JCIS%"  -o "%OUTPUT_DIR%/%JCIS%"  -p  %JCIS_PDF_PATTERN% -x %JCIS_XLSX%  %JCIS_INIT_ARGS%  || GOTO FAIL
 ECHO Initializing PROLE
-java -jar target/%JAR% %COMMON_OPTS% -a %PROLE% -i "%INPUT_DIR%/%PROLE%" -o "%OUTPUT_DIR%/%PROLE%" -p %PROLE_PDF_PATTERN% -x %PROLE_XLSX% %PROLE_INIT_ARGS% || GOTO FAIL
+java %JAVA_OPTS% -jar target/%JAR% %COMMON_OPTS% -a %PROLE% -i "%INPUT_DIR%/%PROLE%" -o "%OUTPUT_DIR%/%PROLE%" -p %PROLE_PDF_PATTERN% -x %PROLE_XLSX% %PROLE_INIT_ARGS% || GOTO FAIL
 GOTO END
 
 :LIST
 SET COMMON_OPTS=list -p -n -e -o
 ECHO Listing JISBD
-java -jar target/%JAR% %COMMON_OPTS% -f %JISBD_EDITION_FILE% || GOTO FAIL
+java %JAVA_OPTS% -jar target/%JAR% %COMMON_OPTS% -f %JISBD_EDITION_FILE% || GOTO FAIL
 ECHO Listing JCIS
-java -jar target/%JAR% %COMMON_OPTS% -f %JCIS_EDITION_FILE%  || GOTO FAIL
+java %JAVA_OPTS% -jar target/%JAR% %COMMON_OPTS% -f %JCIS_EDITION_FILE%  || GOTO FAIL
 ECHO Listing PROLE
-java -jar target/%JAR% %COMMON_OPTS% -f %PROLE_EDITION_FILE% || GOTO FAIL
+java %JAVA_OPTS% -jar target/%JAR% %COMMON_OPTS% -f %PROLE_EDITION_FILE% || GOTO FAIL
 GOTO END
 
 
@@ -71,13 +73,13 @@ CHOICE /C YN /M "We're going to modify %DS_URI%. Continue?"
 IF %ERRORLEVEL% == 2 GOTO ABORT
 SET COMMON_OPTS=sync-authors -i -u %DS_URI% -e %DS_EMAIL% -p %DS_PASSWORD% -a
 ECHO Synchronizing JISBD authors 
-java -jar target/%JAR% %COMMON_OPTS% -f %JISBD_EDITION_FILE%                      || GOTO FAIL
+java %JAVA_OPTS% -jar target/%JAR% %COMMON_OPTS% -f %JISBD_EDITION_FILE%                      || GOTO FAIL
 ECHO Synchronizing JCIS authors 
-java -jar target/%JAR% %COMMON_OPTS% -f  %JCIS_EDITION_FILE%                      || GOTO FAIL
+java %JAVA_OPTS% -jar target/%JAR% %COMMON_OPTS% -f  %JCIS_EDITION_FILE%                      || GOTO FAIL
 ECHO Synchronizing PROLE authors 
-java -jar target/%JAR% %COMMON_OPTS% -f %PROLE_EDITION_FILE%                      || GOTO FAIL
+java %JAVA_OPTS% -jar target/%JAR% %COMMON_OPTS% -f %PROLE_EDITION_FILE%                      || GOTO FAIL
 ECHO Curating all authors 
-java -jar target/%JAR% curate-authors -u %DS_URI% -e %DS_EMAIL% -p %DS_PASSWORD%  || GOTO FAIL
+java %JAVA_OPTS% -jar target/%JAR% curate-authors -u %DS_URI% -e %DS_EMAIL% -p %DS_PASSWORD%  || GOTO FAIL
 GOTO END
 
 :PUBLISH
@@ -86,31 +88,31 @@ CHOICE /C YN /M "We're going to modify %DS_URI%. Continue?"
 IF %ERRORLEVEL% == 2 GOTO ABORT
 SET COMMON_OPTS=publish -u %DS_URI% -e %DS_EMAIL% -p %DS_PASSWORD% -a -c
 ECHO Publishing JISBD proceedings 
-java -jar target/%JAR% %COMMON_OPTS% -f %JISBD_EDITION_FILE% || GOTO FAIL
+java %JAVA_OPTS% -jar target/%JAR% %COMMON_OPTS% -f %JISBD_EDITION_FILE% || GOTO FAIL
 ECHO Publishing JCIS proceedings
-java -jar target/%JAR% %COMMON_OPTS% -f  %JCIS_EDITION_FILE% || GOTO FAIL
+java %JAVA_OPTS% -jar target/%JAR% %COMMON_OPTS% -f  %JCIS_EDITION_FILE% || GOTO FAIL
 ECHO Publishing PROLE proceedings 
-java -jar target/%JAR% %COMMON_OPTS% -f %PROLE_EDITION_FILE% || GOTO FAIL
+java %JAVA_OPTS% -jar target/%JAR% %COMMON_OPTS% -f %PROLE_EDITION_FILE% || GOTO FAIL
 GOTO END
 
 :VALIDATE
 SET COMMON_OPTS=validate
 ECHO Validating JISBD 
-java -jar target/%JAR% %COMMON_OPTS% -f %JISBD_EDITION_FILE% || GOTO FAIL
+java %JAVA_OPTS% -jar target/%JAR% %COMMON_OPTS% -f %JISBD_EDITION_FILE% || GOTO FAIL
 ECHO Validating JCIS 
-java -jar target/%JAR% %COMMON_OPTS% -f %JCIS_EDITION_FILE%  || GOTO FAIL
+java %JAVA_OPTS% -jar target/%JAR% %COMMON_OPTS% -f %JCIS_EDITION_FILE%  || GOTO FAIL
 ECHO Validating PROLE 
-java -jar target/%JAR% %COMMON_OPTS% -f %PROLE_EDITION_FILE% || GOTO FAIL
+java %JAVA_OPTS% -jar target/%JAR% %COMMON_OPTS% -f %PROLE_EDITION_FILE% || GOTO FAIL
 GOTO END
 
 :DISCARD
-SET COMMON_OPTS=discard-uuids -a -e -p -s -t
+SET COMMON_OPTS=discard-uuids -e -p -s -t
 ECHO Discarding JISBD UUIDs 
-java -jar target/%JAR% %COMMON_OPTS% -f %JISBD_EDITION_FILE% || GOTO FAIL
+java %JAVA_OPTS% -jar target/%JAR% %COMMON_OPTS% -f %JISBD_EDITION_FILE% || GOTO FAIL
 ECHO Discarding JCIS UUIDs 
-java -jar target/%JAR% %COMMON_OPTS% -f %JCIS_EDITION_FILE%  || GOTO FAIL
+java %JAVA_OPTS% -jar target/%JAR% %COMMON_OPTS% -f %JCIS_EDITION_FILE%  || GOTO FAIL
 ECHO Discarding PROLE UUIDs 
-java -jar target/%JAR% %COMMON_OPTS% -f %PROLE_EDITION_FILE% || GOTO FAIL
+java %JAVA_OPTS% -jar target/%JAR% %COMMON_OPTS% -f %PROLE_EDITION_FILE% || GOTO FAIL
 GOTO END
 
 :FAIL
